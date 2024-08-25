f7ca7bb0042c3b9bb68f3c8a36e07c2f6fc86661
BravoLuciano <122758532+BravoLuciano@users.noreply.github.com>
2024-08-25 09:46:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionarTarefa() {
+  let valorDoinput = document.querySelector("input").value;
+
+  let li = document.createElement("Li");
+  li.innerHTML = valorDoinput + '<span onclick="deletarTarefa(this)">❌</span>';
+
+  document.querySelector("ul").appendChild(li);
+
+  document.querySelector("input").value = "";
+}
+
+function deletarTarefa(li) {
+  li.parentElement.remove();
+}
